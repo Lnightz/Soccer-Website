8943ace39bf265ba4385f0a66621fe6148ac1eae
Lnightz <lightzzero@gmail.com>
2020-04-22 00:29:57 +0700

    Fix navigation bar

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ $(".slider-one")
     .not(".slick-intialized")
     .slick({
         dots: true,
-        autoplay:true,
-        autoplaySpeed: 3000,
+        autoplay: true,
+        autoplaySpeed: 2000,
         prevArrow :".site-slider .slider-btn .prev",
         nextArrow :".site-slider .slider-btn .next",
         
