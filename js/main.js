2a9f2b87eb074d4689533637cdc80e37c7f25b42
Lnightz <53927056+Lnightz@users.noreply.github.com>
2020-04-28 10:40:23 +0700

    Card_v1

--- js/main.js
+++ js/main.js
@@ -9,3 +9,13 @@ $(".slider-one")
         nextArrow :".site-slider .slider-btn .next",
         
     });
+
+/** Second Slider */
+// $(".site-slider-two")
+//     .not(".slick-intialized")
+//     .slick({
+//         prevArrow :".site-slider-two .slider-btn .prev",
+//         nextArrow :".site-slider-two .slider-btn .next",
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//     });
