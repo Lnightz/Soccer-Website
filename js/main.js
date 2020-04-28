c4878b74bf1a712f767d12f7b2cbc5127a7e0f40
Vuong Hieu <lightzzero@gmail.com>
2020-04-28 23:04:37 +0700

    Fix slider responsive

--- js/main.js
+++ js/main.js
@@ -19,6 +19,33 @@ $('.hot-class-wrapper')
     slidesToScroll: 1,
     prevArrow: ".hot-class-slider .slider-btn .prev",
     nextArrow: ".hot-class-slider .slider-btn .next",
+    responsive: [
+        {
+          breakpoint: 1600,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 1280,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 900,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
 });
 
       
